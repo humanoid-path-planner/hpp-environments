ffe491dfe8b8705260ec37ad1ae546f9f0ccbb5a
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 11:24:56 +0200

    fix v8.0.0 merge

--- flake.nix
+++ flake.nix
@@ -1,42 +1,28 @@
 {
   description = "Environments and robot descriptions for HPP";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            gazebros2nix.overrides.hpp-environments = _final: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./examples
-                  ./meshes
-                  ./package.xml
-                  ./src
-                  ./srdf
-                  ./texture
-                  ./urdf
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-environments = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./examples
+              ./meshes
+              ./package.xml
+              ./src
+              ./srdf
+              ./texture
+              ./urdf
+            ];
+          };
+        };
       }
     );
 }
